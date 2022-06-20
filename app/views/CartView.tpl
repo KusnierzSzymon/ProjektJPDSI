c88df763f7d47f914e4de8edfcd0b4470ed77273
KusnierzSzymon <szymon200022@o2.pl>
2022-06-21 00:22:06 +0200

    Siódmy

--- app/views/CartView.tpl
+++ app/views/CartView.tpl
@@ -4,9 +4,7 @@
         <div class="container">
             <ol class="breadcrumb">
                 <li><a href="{$conf->action_root}main">Strona główna</a></li>
-                <li class="active">{$page_title}</li>
-            </ol>
-               <h2 class="text-center thin">Koszyk</b></h2>
+                
             
                 <p>Przejrzyj zawartość swojego koszyka przed finalizacją zamówienia.</p>
                 <table>
@@ -19,13 +17,13 @@
                     {foreach $cart as $row}
               				<tr>
               					<td>{$row["Id_sprzet"]}</td>
-												<td>{$row["Id_wypozyczenia"]}</td>
+		       <td>{$row["Id_wypozyczenia"]}</td>
                         <td>
-														<a href="{$conf->action_url}cartDelete/{$row['Id_sprzet']}" class="button alt small">Usuń</a>
+			<a href="{$conf->action_url}cartDelete/{$row['Id_sprzet']}" class="button alt small">Usuń</a>
                         </td>
               				</tr>
-										{foreachelse}
-											<h2>Pusto</h2>
+			{foreachelse}
+						<h2>Pusto</h2>
                       <p>Przejdz do listy sprzętu!</p>
               			{/foreach}
                   </tbody>
@@ -35,21 +33,18 @@
 				</div>
         <div class="col-4 col-12-medium">
 
-					<!-- Sidebar -->
-          <form action="{$conf->action_root}search" method="post">
-            <div id="sidebar">
-							<section class="widget thumbnails">
-                  <h2>Zakup</h2>
+		
+							
+                    <h2>Zakup</h2>
                   <p>Suma zakupów:&nbsp;<strong>120,00 zł‚</strong></p>
                   <p>Odbierz zarezerwowany sprzęt w naszej wypożyczalni!</p>
-  								<div class="grid">
+  								
                     <a href="{$conf->action_root}cartFinalize" class="button icon fa-file-alt">Zakup</a>
-  								</div>
-							</section>
-						</div>
-          </form>
+  								
+	
 				</div>
 			</div>
-		</div>
+</div>
+		
 	</div>
 {/block}
--- public/templates_c/48f58b1b392b8be758c7e41039cc5ad6adc9049a_0.file.CartView.tpl.php
+++ public/templates_c/48f58b1b392b8be758c7e41039cc5ad6adc9049a_0.file.CartView.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.1.0, created on 2022-06-20 19:49:40
+/* Smarty version 4.1.0, created on 2022-06-21 00:02:39
   from 'C:\xampp\htdocs\wypozyczalnia\app\views\CartView.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.1.0',
-  'unifunc' => 'content_62b0b334a620c6_78391267',
+  'unifunc' => 'content_62b0ee7fc5f103_79257694',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '48f58b1b392b8be758c7e41039cc5ad6adc9049a' => 
     array (
       0 => 'C:\\xampp\\htdocs\\wypozyczalnia\\app\\views\\CartView.tpl',
-      1 => 1655747379,
+      1 => 1655762558,
       2 => 'file',
     ),
   ),
@@ -20,24 +20,24 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_62b0b334a620c6_78391267 (Smarty_Internal_Template $_smarty_tpl) {
+function content_62b0ee7fc5f103_79257694 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_loadInheritance();
 $_smarty_tpl->inheritance->init($_smarty_tpl, true);
 ?>
 
 <?php 
-$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_139075870562b0b334a51517_68411228', 'intro');
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_148651188562b0ee7fc54656_21713243', 'intro');
 ?>
 
 <?php $_smarty_tpl->inheritance->endChild($_smarty_tpl, "main.tpl");
 }
 /* {block 'intro'} */
-class Block_139075870562b0b334a51517_68411228 extends Smarty_Internal_Block
+class Block_148651188562b0ee7fc54656_21713243 extends Smarty_Internal_Block
 {
 public $subBlocks = array (
   'intro' => 
   array (
-    0 => 'Block_139075870562b0b334a51517_68411228',
+    0 => 'Block_148651188562b0ee7fc54656_21713243',
   ),
 );
 public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
@@ -48,10 +48,7 @@ public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
             <ol class="breadcrumb">
                 <li><a href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
 main">Strona główna</a></li>
-                <li class="active"><?php echo $_smarty_tpl->tpl_vars['page_title']->value;?>
-</li>
-            </ol>
-               <h2 class="text-center thin">Koszyk</b></h2>
+                
             
                 <p>Przejrzyj zawartość swojego koszyka przed finalizacją zamówienia.</p>
                 <table>
@@ -70,19 +67,19 @@ $_smarty_tpl->tpl_vars['row']->do_else = false;
               				<tr>
               					<td><?php echo $_smarty_tpl->tpl_vars['row']->value["Id_sprzet"];?>
 </td>
-												<td><?php echo $_smarty_tpl->tpl_vars['row']->value["Id_wypozyczenia"];?>
+		       <td><?php echo $_smarty_tpl->tpl_vars['row']->value["Id_wypozyczenia"];?>
 </td>
                         <td>
-														<a href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
+			<a href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_url;?>
 cartDelete/<?php echo $_smarty_tpl->tpl_vars['row']->value['Id_sprzet'];?>
 " class="button alt small">Usuń</a>
                         </td>
               				</tr>
-										<?php
+			<?php
 }
 if ($_smarty_tpl->tpl_vars['row']->do_else) {
 ?>
-											<h2>Pusto</h2>
+						<h2>Pusto</h2>
                       <p>Przejdz do listy sprzętu!</p>
               			<?php
 }
@@ -94,24 +91,19 @@ $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
 				</div>
         <div class="col-4 col-12-medium">
 
-					<!-- Sidebar -->
-          <form action="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
-search" method="post">
-            <div id="sidebar">
-							<section class="widget thumbnails">
-                  <h2>Zakup</h2>
-                  <p>Suma zakupów:&nbsp;<strong>20,00 zł‚</strong></p>
-                  <p>PamiÄ™taj: klikniÄ™cie Zakup spowoduje jedynie rezerwacjÄ™ wybranych produktĂłw. MoĹĽna bÄ™dzie je odebraÄ‡ osobiĹ›cie w odpowiednich wypoĹĽyczalniach.</p>
-  								<div class="grid">
+		
+							
+                    <h2>Zakup</h2>
+                  <p>Suma zakupów:&nbsp;<strong>120,00 zł‚</strong></p>
+                  <p>Odbierz zarezerwowany sprzęt w naszej wypożyczalni!</p>
+  								
                     <a href="<?php echo $_smarty_tpl->tpl_vars['conf']->value->action_root;?>
 cartFinalize" class="button icon fa-file-alt">Zakup</a>
-  								</div>
-							</section>
-						</div>
-          </form>
+  								
+	
 				</div>
-			</div>
-		</div>
+			
+		
 	</div>
 <?php
 }
